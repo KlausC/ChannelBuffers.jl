581ee64840c2569005e71408778087b8b4ae1aa9
Klaus Crusius <klaus.crusius@web.de>
2020-12-04 11:21:29 +0100

    refactored

--- src/tasks.jl
+++ src/tasks.jl
@@ -1,53 +1,41 @@
 
-struct BufferTaskDescription{F<:Function,Args<:Tuple}
+struct BClosure{F<:Function,Args<:Tuple}
     f::F
     args::Args
-    BufferTaskDescription(f::F, args...) where F = new{F,typeof(args)}(f, args) 
+    BClosure(f::F, args...) where F = new{F,typeof(args)}(f, args) 
 end
-
 
 const UIO = Union{IO,AbstractString}
 
-struct BufferTask{In<:UIO,Out<:UIO}
-    cin::In
-    cout::Out
-    task::Ref{Task}
-    function BufferTask(cin::In, cout::Out) where {In,Out}
-        new{In,Out}(cin, cout, Ref{Task}())
-    end
-end
-
-struct BufferTaskList
-    list::Vector{<:BufferTaskDescription}
+struct BClosureList
+    list::Vector{<:BClosure}
 end
 
 import Base: |
-function |(src::BufferTaskDescription, bt::BufferTaskDescription)
-    BufferTaskList([src, bt])
+function |(src::BClosure, btd::BClosure)
+    BClosureList([src, btd])
 end
-function |(src::BufferTaskList, bt::BufferTaskDescription)
-    BufferTaskList(vcat(src.list, bt))
+function |(src::BClosureList, btd::BClosure)
+    BClosureList(vcat(src.list, btd))
 end
     
-function task!(btd::BufferTaskDescription, bt::BufferTask)
+function task!(btd::BClosure, cin, cout)
     function task_function()
         try
-            btd.f(bt, btd.args...)
+            btd.f(cin, cout, btd.args...)
         finally
-            bt.cout isa ChannelIO && close(bt.cout)
+            cout isa ChannelIO && close(cout)
         end
     end
     Task(task_function)
 end
 
 function _schedule(s, cin, cout)
-    bt = BufferTask(cin, cout)
-    t = task!(s, bt)
-    bt.task[] = t
+    t = task!(s, cin, cout)
     schedule(t)
 end
 
-function Base.schedule(btdl::BufferTaskList; stdin=stdin, stdout=stdout)
+function Base.schedule(btdl::BClosureList; stdin=stdin, stdout=stdout)
     n = length(btdl.list)
     tl = Vector{Task}(undef, n)
     s = btdl.list[n]
@@ -77,93 +65,93 @@ export Destination, Tarx
 const DEFAULT_READ_BUFFER_SIZE = DEFAULT_BUFFER_SIZE
 
 function Tarc(dir::AbstractString)
-    function _tarc(bt::BufferTask, dir::AbstractString)
-        Tar.create(dir, bt.cout)
+    function _tarc(cin::IO,cout::IO, dir::AbstractString)
+        Tar.create(dir, cout)
     end
-    BufferTaskDescription(_tarc, dir)
+    BClosure(_tarc, dir)
 end
 
 function Tarx(dir::AbstractString)
-    function _tarx(bt::BufferTask, dir::AbstractString)
-        Tar.extract(bt.cin, dir)
+    function _tarx(cio::IO, cout::IO, dir::AbstractString)
+        Tar.extract(cin, dir)
     end
-    BufferTaskDescription(_tarx, dir)
+    BClosure(_tarx, dir)
 end
 
 function Download(url::AbstractString)
-    function _download(bt::BufferTask, url::AbstractString)
-        Downloads.download(url, bt.cout)
+    function _download(cin::IO, cout::IO, url::AbstractString)
+        Downloads.download(url, cout)
     end
-    BufferTaskDescription(_download, url)
+    BClosure(_download, url)
 end
 
 function Gunzip()
-    function _gunzip(bt::BufferTask)
-        tc = GzipDecompressorStream(bt.cin)
+    function _gunzip(cin::IO, cout::IO)
+        tc = GzipDecompressorStream(cin)
         buffer = Vector{UInt8}(undef, DEFAULT_READ_BUFFER_SIZE)
         while !eof(tc)
             n = readbytes!(tc, buffer)
-            write(bt.cout, view(buffer, 1:n))
+            write(cout, view(buffer, 1:n))
         end
     end
-    BufferTaskDescription(_gunzip)
+    BClosure(_gunzip)
 end
 
 function Gzip()
-    function _gzip(bt::BufferTask)
-        tc = GzipCompressorStream(bt.cin)
+    function _gzip(cin::IO, cout::IO)
+        tc = GzipCompressorStream(cin)
         buffer = Vector{UInt8}(undef, DEFAULT_READ_BUFFER_SIZE)
         while !eof(tc)
             n = readbytes!(tc, buffer)
-            write(bt.cout, view(buffer, 1:n))
+            write(cout, view(buffer, 1:n))
         end
     end
-    BufferTaskDescription(_gzip)
+    BClosure(_gzip)
 end
 
 function Transcode(codec::Codec)
-    function _transcode(bt::BufferTask)
+    function _transcode(cin::IO, cout::IO)
         buffer = Vector{UInt8}(undef, DEFAULT_READ_BUFFER_SIZE * 10)
         TranscodingStreams.initialize(codec)
-        while !eof(bt.cin)
-            n = readbytes!(bt.cin, buffer)
+        while !eof(cin)
+            n = readbytes!(cin, buffer)
             r = transcode(codec, buffer[1:n])
-            write(bt.cout, r)
+            write(cout, r)
         end
-        flush(bt.cout)
+        flush(cout)
         TranscodingStreams.finalize(codec)
     end
-    BufferTaskDescription(_transcode)
+    BClosure(_transcode)
 end   
 
 function Source(src::UIO)
-    function _source(bt::BufferTask, src::UIO)
+    function _source(cin::IO, cout::IO, src::UIO)
         io = src isa IO ? src : io = open(src, "r")
         try
             buffer = Vector{UInt8}(undef, DEFAULT_READ_BUFFER_SIZE)
             while !eof(io)
                 n = readbytes!(io, buffer)
-                write(bt.cout, view(buffer, 1:n))
+                write(cout, view(buffer, 1:n))
             end
         finally
             src isa IO || close(io)
         end
     end
-    BufferTaskDescription(_source, src)
+    BClosure(_source, src)
 end
 
 function Destination(dst::UIO)
-    function _destination(bt::BufferTask, dst::UIO)
+    function _destination(cin::IO, cout::IO, dst::UIO)
         buffer = Vector{UInt8}(undef, DEFAULT_READ_BUFFER_SIZE)
         io = dst isa IO ? dst : open(dst, "w")
         try
-            while !eof(bt.cin)
-                n = readbytes!(bt.cin, buffer)
+            while !eof(cin)
+                n = readbytes!(cin, buffer)
                 write(io, view(buffer, 1:n))
             end
         finally
             dst isa IO || close(io)
         end
     end
-    BufferTaskDescription(_destination, dst)
+    BClosure(_destination, dst)
 end
