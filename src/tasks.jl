95376602de3ca098789bfa9b87bfcf0d331af6ff
Klaus Crusius <klaus.crusius@web.de>
2020-12-05 21:34:58 +0100

    introduce `BTask`

--- src/tasks.jl
+++ src/tasks.jl
@@ -22,9 +22,19 @@ struct BClosureList{In,Out}
 end
 BClosureList(list) = BClosureList(list, stdin, stdout)
 
+struct BTask{X}
+    task::Task
+end
+Base.show(io::IO, m::MIME"text/plain", bt::BTask) = show(io, m, bt.task)
+Base.fetch(bt::BTask) = fetch(bt.task)
+Base.wait(bt::BTask) = wait(bt.task)
+Base.istaskstarted(bt::BTask) = istaskstarted(bt.task)
+Base.istaskdone(bt::BTask) = istaskdone(bt.task)
+Base.istaskfailed(bt::BTask) = istaskfailed(bt.task)
+
 # List of tasks - output of `schedule`
-struct BTaskList
-    list::Vector{<:Task}
+struct BTaskList{V<:Vector{<:BTask}}
+    list::V
 end
 
 import Base: |, <, >
@@ -46,10 +56,12 @@ end
 
 Wait for the last task in the list to finish.
 """
-function Base.wait(tv::BTaskList)
-    if length(tv.list) > 0
-        wait(last(tv.list))
-    end
+function Base.wait(tv::BTaskList, ix::Integer=0)
+    n = length(tv)
+    i = ix == 0 ? n : Int(ix)
+    0 < n || return nothing
+    0 < i <= n || throw(BoundsError(tv.list, i))
+    @inbounds wait(tv.list[i])
 end
 
 """
@@ -58,9 +70,12 @@ end
 Wait for last Task in to finish, then return its result value.
 If the task fails with an exception, a `TaskFailedException` (which wraps the failed task) is thrown.
 """
-function Base.fetch(tv::BTaskList)
-    length(tv.list) > 0 || throw(ArgumentError("cannot fetch from empty task list"))
-    fetch(last(tv.list))
+function Base.fetch(tv::BTaskList, ix::Integer=0)
+    n = length(tv)
+    i = ix == 0 ? n : Int(ix)
+    0 < n || throw(ArgumentError("cannot fetch from empty task list"))
+    0 < i <= n || throw(BoundsError(tv.list, i))
+    @inbounds fetch(tv.list[i])
 end
 
 Base.length(tv::BTaskList) = length(tv.list)
@@ -72,13 +87,12 @@ Base.show(io::IO, m::MIME"text/plain", tv::BTaskList) = show(io, m, tv.list)
 
 Access the argumentless function provided to the task
 """
-function task_code(t::Task)
-    isdefined(t.code, :task_function) ? t.code.task_function : t.code
-end
-task_cin(t::Task) = task_code(t).cin
-task_cout(t::Task) = task_code(t).cout
-task_function(t::Task) = task_code(t).btd.f
-task_args(t::Task) = task_code(t).btd.args
+task_code(t::BTask{:Task}) = t.task.code
+task_code(t::BTask{:Threat}) = t.code.task_function
+task_cin(t::BTask) = task_code(t).cin
+task_cout(t::BTask) = task_code(t).cout
+task_function(t::BTask) = task_code(t).btd.f
+task_args(t::BTask) = task_code(t).btd.args
 
 """
     run(BClosure; stdin=stdin, stdout=stdout)
@@ -95,6 +109,7 @@ end
 Start all parallel tasks defined in list, io redirection defaults are defined in the list
 """
 function Base.run(btdl::BClosureList)
+    T = use_tasks() ? :Task : :Threat
     stdin = btdl.cin
     stdout = btdl.cout
     n = length(btdl.list)
@@ -111,7 +126,7 @@ function Base.run(btdl::BClosureList)
         t = _schedule(s, cin, cout)
         tl[i] = t
     end
-    BTaskList(tl)    
+    BTaskList(BTask{T}.(tl))
 end
 
 """
@@ -123,6 +138,10 @@ It may be wrapped in an argumentless closure to be used in a `Task` definition.
 """
 function closure(f::Function, args...)
     BClosure(f, args)
+end
+
+function use_tasks()
+    Threads.nthreads() <= 1 || Threads.threadid() != 1
 end
 
 # schedule single task
