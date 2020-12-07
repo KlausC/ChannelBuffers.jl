4c69471163b0932cf05f356274c8c3d1d23f62c1
Klaus Crusius <klaus.crusius@web.de>
2020-12-07 10:02:37 +0100

    renamed file

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,6 @@ using TranscodingStreams, CodecZlib
 # reminder: @__MODULE__() === Main to check if executed by CI
 
 @testset "ChannelBuffers" begin
-    @testset "ChannelIO" begin include("channelIO.jl") end 
+    @testset "ChannelIO" begin include("channelio.jl") end 
     @testset "tasks    " begin include("tasks.jl") end
 end
