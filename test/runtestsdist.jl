e21c69470a80f241a26ecc716d2f7a8e654a0405
KlausC <klausC@users.noreply.github.com>
2023-05-18 11:56:50 +0200

    test distributed

--- test/runtestsdist.jl
+++ test/runtestsdist.jl
@@ -5,13 +5,16 @@ addprocs(2)
 PROCS = procs()
 P2 = PROCS[end-1]
 P3 = PROCS[end]
+println("procs = ", PROCS)
 #addprocs([("127.0.0.1", 1)])
+#=
 @everywhere begin
     using Pkg
     Pkg.activate("..")
 end
 println(fetch(@spawnat(P2, begin io = IOBuffer(); Pkg.status(;io); String(take!(io)); end)))
-@everywhere begin
+=#
+@everywhere P2, P3 begin
     using ChannelBuffers
 end
 
